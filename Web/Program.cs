b0ecb9547531bd97d87c6f4281420b6f385667aa
Nicky Waites <nicky.waites@xodusgroup.com>
2021-04-15 21:13:39 +0100

    Initial commit of nickywaites.com statiq site

---
+++ Web/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace Web
+{
+    class Program
+    {
+
+        public static async Task<int> Main(string[] args)
+        {
+            // Ensure consistent date handling
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-GB");
+
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+        }
+
+    }
+
+    public class SocialViewModel
+    {
+        public string Social { get; set; }
+    }
+
+}
